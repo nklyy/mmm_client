93e899cdcfef3711668a22f53d2f25b3ed13d6bf
nklyy <44751173+nklyy@users.noreply.github.com>
2021-07-07 21:22:25 +0300

    add context fix grid and button styles

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
 
 import '../../styles/main.css';
 import './Home.css';
+import ChooseFrom from '../ChooseFrom/ChooseFrom';
 
 function Home() {
+  const [cf, setSf] = useState(false)
+
+  const handleClick = () => {
+    setSf(true)
+  }
+
   return (
     <>
       <div className="min-h-screen p-2 flex flex-col justify-center items-center ">
         <div className="m-1.5">
-          <Link to={'/cf'}>
-            <button className="btn bt">Let`s start!</button>
-          </Link>
+          {/*<Link to={'/cf'}>*/}
+          {/*  <button className="btn bt">Let`s start!</button>*/}
+          {/*</Link>*/}
+          {cf ? <ChooseFrom/> : <button onClick={handleClick} className="btn bt">Let`s start!</button>}
         </div>
       </div>
     </>
